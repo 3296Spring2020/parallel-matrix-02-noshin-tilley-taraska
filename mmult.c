6fe054de0b0964614ca1d5594b73ddb6c4b41e9d
DTaraska11 <56418437+DTaraska11@users.noreply.github.com>
2020-03-09 20:35:34 -0400

    Updated mmult for part 5

--- mmult.c
+++ mmult.c
@@ -24,14 +24,16 @@ int mmult(double *c,
 	      double *a, int aRows, int aCols, 
 	      double *b, int bRows, int bCols) {
 
-    for(int i = 0; i < aRows; ++i) {
-        for(int j = 0; j < bCols; ++j) {
-            c[i * bCols + j] = 0;
-            for(int k = 0; k < aRows; ++k) {
-                c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
-            }
-        }
+    for (int i = 0; i < aRows; i++) {
+    for (int j = 0; j < bCols; j++) {
+      c[i*bCols + j] = 0;
     }
+    for (int k = 0; k < aCols; k++) {
+      for (j = 0; j < bCols; j++) {
+	c[i*bCols + j] += a[i*aCols + k] * b[k*bCols + j];
+      }
+    }
+  }
 
   return 0;
 }
