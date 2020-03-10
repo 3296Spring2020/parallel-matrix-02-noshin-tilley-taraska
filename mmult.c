5fc1fd209cf05dc09afd3c7d318330b580958aaa
Jack Tilley <47064848+Jack-Tilley@users.noreply.github.com>
2020-03-09 20:49:01 -0400

    Update mmult.c

--- mmult.c
+++ mmult.c
@@ -20,66 +20,6 @@
  * @param bCols : the number of columns in b.
  * @return 0 if the matrix multiplication is successful.
  */
-//// init
-//int mmult(double *c,
-//	      double *a, int aRows, int aCols,
-//	      double *b, int bRows, int bCols) {
-//
-//    for(int i = 0; i < aRows; ++i) {
-//        for(int j = 0; j < bCols; ++j) {
-//            c[i * bCols + j] = 0;
-//            for(int k = 0; k < aRows; ++k) {
-//                c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
-//            }
-//        }
-//    }
-//
-//  return 0;
-//}
-//// copied
-//int mmult(double *c,
-//          double *a, int aRows, int aCols,
-//          double *b, int bRows, int bCols) {
-//
-//    for (int i = 0; i < aRows; i++) {
-//        for (int j = 0; j < bCols; j++)
-//            c[i][j] = 0;
-//        for (int k = 0; k < aRows; k++)
-//            for (int l = 0; l < bCols; l++)
-//                c[i][l] += a[i][k] * b[k][l];
-//    }
-//    return 0;
-//}
-//// test
-// int mmult(double *c,
-//           double *a, int aRows, int aCols,
-//           double *b, int bRows, int bCols) {
-
-//     for (int i = 0; i < aRows; i++) {
-//         int j = 0;
-//         for (j = 0; j < bCols; j++)
-//             c[i * bCols + j] = 0;
-//         for (int k = 0; k < aRows; k++)
-//             for (int l = 0; l < bCols; l++)
-//                 c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
-//     }
-//     return 0;
-// }
-//Test2
-// int mmult(double *c,
-//           double *a, int aRows, int aCols,
-//           double *b, int bRows, int bCols) {
-//     int N = aRows;
-//     for (int i = 0; i < N; i++) {
-//         int j = 0;
-//         for (j = 0; j < N; j++)
-//             c[i * bCols + j] = 0;
-//         for (int k = 0; k < N; k++)
-//             for (int l = 0; l < N; l++)
-//                 c[i * N + j] += a[i * N + k] * b[k * N + j];
-//     }
-//     return 0;
-// }
 
 ////last
 int mmult(double *c,
