3a05619cc8653b698483449ff8cf7397b6f5b764
Sarah Noshin <tuf46965@temple.edu>
2020-03-18 12:12:26 -0400

    Update mmult_omp_timing.c

--- mmult_omp_timing.c
+++ mmult_omp_timing.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * This file compares the time to multiply matrices
  * both unoptimized and with OMP. 
@@ -8,16 +9,6 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include "mat.h"
-
-
-int mmult(double *c, double *a, int aRows, int aCols, double *b, int bRows, int bCols);
-int mmult_omp(double *c,double *a, int aRows, int aCols,double *b, int bRows, int bCols);
-
-double* gen_matrix(int n, int m);
-// was a void comp_mat earlier
-int compare_matrices(double* a, double* b, int nRows, int nCols);
-
-// double delta time was here
 
 
 int main(int argc, char* argv[]) {
@@ -43,16 +34,8 @@ int main(int argc, char* argv[]) {
     times[1] = ((double)t)/CLOCKS_PER_SEC;
     printf(" %f", times[1]);
     printf("\n");
-
-         printf("%d %f", n, times[0]);
-    t = clock();
-    mmult_omp(c2, a, n, n, b, n, n);
-    t = clock() - t;
-    times[1] = ((double)t)/CLOCKS_PER_SEC;
-    printf(" %f", times[1]);
-    printf("\n");
-
-       
+     printf(" %f", times[1]);
+        printf("\n");
         compare_matrices(c1, c2, n, n);
     } else {
         fprintf(stderr, "Usage %s <n>\n", argv[0]);
@@ -60,4 +43,9 @@ int main(int argc, char* argv[]) {
 }
 
 
-}
+
+   
+
+      
+
+
